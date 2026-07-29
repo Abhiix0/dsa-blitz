b5cd7e11261c03f5aa29796e228675910f2f9687
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-07-29 20:31:58 +0530

    defanging an IP address using replace

--- Week4_StringBasics/DefanginganIPAddress.py
+++ Week4_StringBasics/DefanginganIPAddress.py
@@ -10,4 +10,5 @@ class Solution:
 
         return result
 
+#address.replace(".", "[.]") : this works too, but the above solution is more efficient as it avoids creating multiple intermediate strings.
 #This problem can be solved in O(n) time complexity, where n is the length of the input string. We iterate through the string and check each character. If the character is a dot ('.'), we append '[.]' to the result string; otherwise, we append the character itself. Finally, we return the modified result string.
