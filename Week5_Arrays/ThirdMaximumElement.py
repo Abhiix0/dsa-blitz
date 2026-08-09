63f08b373d60becdbad498b05c48306a4d55d426
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-08-09 11:31:47 +0530

    Third maximum element using traversal

--- Week5_Arrays/ThirdMaximumElement.py
+++ Week5_Arrays/ThirdMaximumElement.py
@@ -8,3 +8,27 @@ class Solution:
         
 # This problem can be solved in O(n log n) time complexity, where n is the length of the input array. We first convert the input list to a set to remove duplicates and then sort the unique elements. If there are at least three unique elements, we return the third maximum element; otherwise, we return the maximum element.
 
+'''
+Optimal solution 
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        first = second = third = None
+
+        for num in nums:
+            if num == first or num == second or num == third:
+                continue
+
+            if first is None or num > first:
+                third = second
+                second = first
+                first = num
+
+            elif second is None or num > second:
+                third = second
+                second = num
+
+            elif third is None or num > third:
+                third = num
+
+        return third if third is not None else first
+'''
