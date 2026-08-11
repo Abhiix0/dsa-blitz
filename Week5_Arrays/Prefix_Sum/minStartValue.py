94fb5694cc951e383fa4713fa6990e87b6dfd272
Abhiix0 <24r21a6778@mlrit.ac.in>
2026-08-11 19:44:08 +0530

    Minimum Value to Get Positive Step by Step Sum

---
+++ Week5_Arrays/Prefix_Sum/minStartValue.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minStartValue(self, nums: list[int]) -> int:
+        current = 0
+        minimum = 0
+
+        for num in nums:
+            current += num
+            minimum = min(minimum, current)
+
+        return 1 - minimum
+
+#this problem can be solved in O(n) time complexity, where n is the length of the input list. We maintain a running sum of the elements in the list and keep track of the minimum value encountered during this process. The minimum starting value required to ensure that the running sum never drops below 1 is calculated as 1 minus the minimum value found.
